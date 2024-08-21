a315b6f36e697fe49dded25739307a63d6d11885
rednaxela1813 <rednaxela1813@gmail.com>
2024-08-21 17:24:51 +0200

    django-tailwind setup. Tailwind classes added to the base template.

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     #my apps
+    "tailwind",
+    "theme",
+    "django_browser_reload",
     "party",
 ]
 
@@ -56,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -130,3 +134,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#Tailwind CSS settings
+TAILWIND_APP_NAME = "theme"
+
+INTERNAL_IPS = [
+    "127.0.0.1,", "localhost"]
+
