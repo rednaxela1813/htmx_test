c25a3d879172ba016378fe4e760f48bd67313420
rednaxela1813 <rednaxela1813@gmail.com>
2024-08-21 11:33:54 +0200

    Models and base template added.

--- party/models.py
+++ party/models.py
@@ -1,7 +1,56 @@
+from os import name
+from tabnanny import verbose
 from django.contrib.auth.models import AbstractUser
+import uuid
+from django.db import models
+from django.conf import settings
+
 
 
 class CustomUser(AbstractUser):
     pass
+
+
+class Party(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    party_date = models.DateField()
+    party_time = models.TimeField()
+    invitation = models.TextField()
+    venue = models.CharField(max_length=200)
+    organizer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="organized_parties")
+
+
+    class Meta:
+        verbose_name_plural = "Parties"
+
+    def __str__(self):
+        return f"{self.venue},  {self.party_date}"
+    
+
+class Gift(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    gift = models.CharField(max_length=200)
+    price = models.FloatField(blank=True, null=True)
+    link = models.URLField(max_length=200, blank=True, null=True)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.gift
+    
+
+class Guest(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=200)
+    attending = models.BooleanField(default=False)
+    party = models.ForeignKey(Party, on_delete=models.CASCADE, related_name="guests")
+
+
+    def __str__(self):
+        return str(self.name)
+    
+
+
+    
+
 
 
